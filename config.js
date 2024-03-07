4a2c1967f30b944fc3164afd0c131114b2da2f4d
ShubhamJain02_ <shubhsiddh@gmail.com>
2024-03-07 16:14:58 +0530

    changes

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
-export const PORT=5555;
+export const PORT=process.env.PORT||5555;
 
 export const mongoDBURL='mongodb+srv://ShubhamJain:S02092003j@cluster0.zpsvkzd.mongodb.net/SecureITLab_database?retryWrites=true&w=majority&appName=Cluster0'
